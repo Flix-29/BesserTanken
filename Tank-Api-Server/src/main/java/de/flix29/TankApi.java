836150cc70acc09753572d52aebfcf48bb04c70d
flix29 <flix.1895@web.de>
2024-03-12 19:42:58 +0100

    PlaTo-60: initial commit

---
+++ Tank-Api-Server/src/main/java/de/flix29/TankApi.java
@@ -0,0 +1,13 @@
+package de.flix29;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TankApi {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TankApi.class, args);
+	}
+
+}
---
+++ Tank-Api-Server/src/test/java/de/flix29/tankApi/StartUpTest.java
@@ -0,0 +1,14 @@
+package de.flix29.tankApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartUpTest {
+
+	@Test
+	void startUp_WithNoErrors() {
+
+	}
+
+}
