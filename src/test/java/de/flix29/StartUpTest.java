3c9267952d86b1836d99886bf83d0d9e37af1313
flix29 <flix.1895@web.de>
2024-04-21 23:38:00 +0200

    PlaTo-1: remove temp test

--- src/test/java/de/flix29/StartUpTest.java
+++ src/test/java/de/flix29/StartUpTest.java
@@ -3,22 +3,12 @@ package de.flix29;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-
 @SpringBootTest
 public class StartUpTest {
 
 	@Test
 	void startUp_WithNoErrors() {
 
-	}
-
-	@Test
-	void loadRepositorySecrets() {
-		String env = System.getenv("API_KEY");
-
-		assertThat(env)
-				.isNotBlank();
 	}
 
 }
