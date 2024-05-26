2133156ba673efe67d194617a85dfa1700410f06
flix29 <flix.1895@web.de>
2024-05-26 14:15:14 +0200

    PlaTo-83: refactor location model

--- src/main/java/de/flix29/besserTanken/model/openDataSoft/Location.java
+++ src/main/java/de/flix29/besserTanken/model/openDataSoft/Location.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.apache.commons.lang3.tuple.Pair;
 
 @Getter
 @Setter
@@ -14,6 +13,7 @@ public class Location {
 
     private int plz;
     private String name;
-    private Pair<Double, Double> coords;
+    private double latitude;
+    private double longitude;
 
 }
