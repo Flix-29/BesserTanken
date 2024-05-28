ad87cbf4b7007173c21806030bd524e11f7915c6
flix29 <flix.1895@web.de>
2024-05-28 23:45:42 +0200

    PlaTo-83: start adding EfficiencyService WIP

--- src/main/java/de/flix29/besserTanken/EfficiencyService.java
+++ src/main/java/de/flix29/besserTanken/EfficiencyService.java
@@ -11,23 +11,19 @@ import java.util.List;
 public class EfficiencyService {
 
     public FuelStation calculateMostEfficientFuelStation(double consumptionPer100Km, List<FuelStation> fuelStations) {
-        var map = new HashMap<String, Double>();
+        var map = new HashMap<FuelStation, Double>();
 
         fuelStations.forEach(fuelStation -> {
-            var consumption = fuelStation.getDistance().doubleValue() * (consumptionPer100Km / 100);
-            var price = fuelStation.getPrice() * consumption;
-            map.put(fuelStation.getId(), price);
+            var consumptionForDistance = fuelStation.getDistance().doubleValue() * (consumptionPer100Km / 100);
+            var priceForDistance = fuelStation.getPrice() * consumptionForDistance;
+            map.put(fuelStation, priceForDistance);
         });
 
         var sorted = map.entrySet().stream()
                 .sorted(Comparator.comparingDouble(HashMap.Entry::getValue))
                 .map(HashMap.Entry::getKey)
-                .toList();
+                .findFirst();
 
-        var first = sorted.get(0);
-
-        List<FuelStation> list = fuelStations.stream().filter(fuelStation -> fuelStation.getId().equals(first)).toList();
-        return list.get(0);
-
+        return sorted.orElse(null);
     }
 }
