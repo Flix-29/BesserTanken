3f2404b56031c10075df69d271741ee691a3f768
flix29 <flix.1895@web.de>
2024-05-29 01:27:37 +0200

    PlaTo-83: deserialize responses from directions-api

---
+++ src/main/java/de/flix29/besserTanken/model/openDataSoft/SimpleLocation.java
@@ -0,0 +1,17 @@
+package de.flix29.besserTanken.model.openDataSoft;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SimpleLocation {
+
+    private double latitude;
+    private double longitude;
+
+}
