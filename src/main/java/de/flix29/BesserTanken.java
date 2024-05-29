43e44b402c771ebb3f25ba422e55c1099d6fe999
flix29 <flix.1895@web.de>
2024-05-29 20:02:37 +0200

    PlaTo-83: refactor secrets

--- src/main/java/de/flix29/BesserTanken.java
+++ src/main/java/de/flix29/BesserTanken.java
@@ -8,15 +8,24 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.env.Environment;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @SpringBootApplication
 @Theme(value = "BesserTanken", variant = Lumo.DARK)
 public class BesserTanken implements AppShellConfigurator {
 
 	@Getter
     private static Environment env;
+	@Getter
+	private static final Map<String, String> secrets = new HashMap<>();
 
 	public static void main(String[] args) {
 		env = SpringApplication.run(BesserTanken.class, args).getEnvironment();
+
+		secrets.put("mapKey", System.getenv().getOrDefault("MAP_KEY", BesserTanken.getEnv().getProperty("map.apikey")));
+		secrets.put("directionsKey", System.getenv().getOrDefault("DIRECTIONS_KEY", BesserTanken.getEnv().getProperty("directions.apikey")));
+		secrets.put("bessertankenKey", System.getenv().getOrDefault("API_KEY", BesserTanken.getEnv().getProperty("bessertanken.apikey")));
 	}
 
 }
