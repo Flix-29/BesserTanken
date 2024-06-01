223cd1a5a03354c63855c9e025561ddbe3771730
flix29 <flix.1895@web.de>
2024-06-01 09:52:39 +0200

    PlaTo-XX: add error logging

--- src/main/java/de/flix29/besserTanken/mapBox/direction/DirectionApiJob.java
+++ src/main/java/de/flix29/besserTanken/mapBox/direction/DirectionApiJob.java
@@ -7,6 +7,8 @@ import de.flix29.BesserTanken;
 import de.flix29.besserTanken.deserializer.CustomRouteDeserializer;
 import de.flix29.besserTanken.model.mapBox.Route;
 import de.flix29.besserTanken.model.openDataSoft.SimpleLocation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -14,6 +16,7 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Collections;
 import java.util.List;
 
 import static de.flix29.besserTanken.deserializer.CustomModelTypes.ROUTE_LIST_TYPE;
@@ -21,6 +24,7 @@ import static de.flix29.besserTanken.deserializer.CustomModelTypes.ROUTE_LIST_TY
 @Service
 public class DirectionApiJob {
 
+    private final Logger LOGGER = LoggerFactory.getLogger(DirectionApiJob.class);
     private final String baseUrl = "https://api.mapbox.com/directions/v5/mapbox/driving-traffic/$startLon$%2C" +
             "$startLat$%3B$endLon$%2C$endLat$?alternatives=true&geometries=geojson&language=en&overview=full&steps=true&access_token=$apiKey$";
     private final Gson gson = new GsonBuilder()
@@ -45,9 +49,13 @@ public class DirectionApiJob {
                 .GET();
 
         var response = HttpClient.newHttpClient().send(requestBuilder.build(), HttpResponse.BodyHandlers.ofString());
-        var jsonArray = gson.fromJson(response.body(), JsonObject.class).get("routes").getAsJsonArray();
 
-        return gson.fromJson(jsonArray, ROUTE_LIST_TYPE);
+        try {
+            var jsonArray = gson.fromJson(response.body(), JsonObject.class).get("routes").getAsJsonArray();
+            return gson.fromJson(jsonArray, ROUTE_LIST_TYPE);
+        } catch (Exception e) {
+            LOGGER.error("Error while parsing JSON: {}", response.body(), e);
+            return Collections.emptyList();
+        }
     }
-
 }
