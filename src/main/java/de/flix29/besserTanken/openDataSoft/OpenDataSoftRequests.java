9cb41626d29b653d43283f1b44cfb11d64f0c254
flix29 <flix.1895@web.de>
2024-06-21 16:22:12 +0200

    PlaTo-XX: fixed error

--- src/main/java/de/flix29/besserTanken/openDataSoft/OpenDataSoftRequests.java
+++ src/main/java/de/flix29/besserTanken/openDataSoft/OpenDataSoftRequests.java
@@ -14,6 +14,7 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Collections;
 import java.util.List;
 
 import static de.flix29.besserTanken.deserializer.CustomModelTypes.LOCATION_TYPE;
